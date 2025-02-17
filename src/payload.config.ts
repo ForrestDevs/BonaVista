48b28de01c18c8a27fa27b7c584145a77ad20c70
ForrestDevs <luke.gannon@me.com>
2025-02-17 14:41:22 -0500

    chore: disable drizzle push on prod

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -111,6 +111,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
     },
+    push: process.env.NODE_ENV === 'production' ? false : true,
     migrationDir: path.resolve(dirname, 'lib/migrations'),
   }),
   collections,
